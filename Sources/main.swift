497c190d692fc95175369f02e1829fcddf386696
Carl Chen <ccf.developer@gmail.com>
2017-01-23 16:43:48 +0800

    add blog update hooker

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,25 @@
-print("Hello, world!")
+import Vapor
+import Foundation
+
+#if os(Linux)
+    typealias CommandProcess = Task
+#else
+    typealias CommandProcess = Process
+#endif
+
+
+
+
+let drop = Droplet()
+
+drop.post("/hooker/updateBlog") { _ in
+
+    let task = CommandProcess()
+    task.launchPath = "/carl/bin/blogRefresh"
+
+    task.launch()
+
+    return JSON([:])
+}
+
+drop.run()
